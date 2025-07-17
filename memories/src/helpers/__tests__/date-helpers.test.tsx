3c8b2c8752b5bb5fddaa5cc95cb88c0e37bbb9bb
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-07-17 13:04:17 +0200

    feat : transform csv to json

--- memories/src/helpers/__tests__/date-helpers.test.tsx
+++ memories/src/helpers/__tests__/date-helpers.test.tsx
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { extractYear } from '../date-helpers.ts';
 
 describe('extractYear', () => {
-  it("transform JJ/MM/AAAA to AAAA", () => {
+  it('transform JJ/MM/AAAA to AAAA', () => {
     expect(extractYear('05/04/1975')).toBe('1975');
     expect(extractYear('29/12/1969')).toBe('1969');
     expect(extractYear('1975')).toBe('1975');
---
+++ memories/src/helpers/format-data-helpers.ts
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import { parse } from 'csv-parse/sync';
+
+export function csvToJson(filePath: string) {
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  return parse(fileContent, {
+    columns: true,
+    skip_empty_lines: true,
+    delimiter: ';',
+    trim: true,
+  });
+}
