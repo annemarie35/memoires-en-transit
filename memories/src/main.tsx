939c26ec0732380643b5fd5c9bbe546ebf2e1027
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-10-17 19:03:22 +0200

    feat: create route for testimonies page

--- memories/src/main.tsx
+++ memories/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
+import { Testimonies } from './pages/Testimonies';
+import { Layout } from './components/Layout';
 import './tailwind.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <App /> },
+      { path: 'temoignages', element: <Testimonies /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
